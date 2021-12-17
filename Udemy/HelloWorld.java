5fb73743562069cf93b867a895cb8e8545fa8978
602115 <602115@stud.hvl.no>
2021-12-17 13:30:12 +0100

    Første utkast, test

---
+++ Udemy/HelloWorld.java
@@ -0,0 +1,13 @@
+package Udemy;
+
+public class HelloWorld {
+
+     public static void main(String[] args) {
+         int x = 5;
+         if (x == 5) {
+             System.out.println("Hei Hedda");
+         }else{
+             System.out.println("Hei Truls");
+         }
+    }
+}
